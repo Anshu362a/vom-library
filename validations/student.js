c113d6f5b034aebefe27c848f8dad152c2a43d6e
Anshu <anshuyadav6246@gmail.com>
2025-07-14 13:43:46 +0530

    fix student dashboard time and student id photo

--- validations/student.js
+++ validations/student.js
@@ -1,4 +1,10 @@
+require('dotenv').config(); // 👈 add this at the very top
+
 const Joi = require('joi');
+
+if (!process.env.ACCESS_CODE) {
+  throw new Error("❌ ACCESS_CODE is not defined in .env file");
+}
 
 module.exports.studentRegisterSchema = Joi.object({
   name: Joi.string().required(),
@@ -13,10 +19,34 @@ module.exports.studentRegisterSchema = Joi.object({
     }),
   confirmPassword: Joi.ref('password'),
   accessCode: Joi.string().valid(process.env.ACCESS_CODE).required()
-
 });
 
 module.exports.studentLoginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required()
 });
+
+
+
+//const Joi = require('joi');
+
+// module.exports.studentRegisterSchema = Joi.object({
+//   name: Joi.string().required(),
+//   email: Joi.string().email().required(),
+//   mobile: Joi.string().length(10).required(),
+//   password: Joi.string()
+//     .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*\\W).{6,}$"))
+//     .required()
+//     .messages({
+//       "string.pattern.base":
+//         "Password must have at least 1 uppercase, 1 lowercase, 1 digit, 1 symbol, and 6+ characters."
+//     }),
+//   confirmPassword: Joi.ref('password'),
+//   accessCode: Joi.string().valid(process.env.ACCESS_CODE).required()
+
+// });
+
+// module.exports.studentLoginSchema = Joi.object({
+//   email: Joi.string().email().required(),
+//   password: Joi.string().required()
+// });
